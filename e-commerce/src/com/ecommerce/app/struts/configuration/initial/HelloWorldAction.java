013dd2839af9908dd5c08037fe2bab0b16ceb1f9
ricardorodriguezroman <ricardorodriguezroman@gmail.com>
2015-08-04 08:02:25 +0200

    Subo el hello world de struts y la plantilla css del modulo de usuario

---
+++ e-commerce/src/com/ecommerce/app/struts/configuration/initial/HelloWorldAction.java
@@ -0,0 +1,25 @@
+package com.ecommerce.app.struts.configuration.initial;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+public class HelloWorldAction extends Action {
+	
+	private static Logger log = Logger.getLogger(HelloWorldAction.class);
+	
+	public ActionForward execute(ActionMapping mapping, ActionForm form, 
+			HttpServletRequest request, HttpServletResponse response)
+	{
+		HelloWorldStrutsForm helloWorldStrutsForm = (HelloWorldStrutsForm) form;
+		helloWorldStrutsForm.setMessage("Hello world!!");
+		log.trace("traza de log de prueba");
+		return mapping.findForward("success");
+	}
+
+}
